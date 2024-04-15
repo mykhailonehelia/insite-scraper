e0ba7036dace494b4c427e2e2f9c617d75639601
Kian Musser <kian@kianmusser.com>
2024-04-15 10:56:45 -0500

    Added logging for each file being processed.

--- tmp/generate.sh
+++ tmp/generate.sh
@@ -12,5 +12,6 @@ mkdir -p "$OUTPUT_DIR"
 cp -r "$TEMPLATE_DIR/"* "$OUTPUT_DIR/"
 
 find "$OUTPUT_DIR" -type f | while read -r file; do
+    echo "processing $file"
     mustache "$JSON_FILE" "$file" > "$file".tmp && mv "$file".tmp "$file"
 done
