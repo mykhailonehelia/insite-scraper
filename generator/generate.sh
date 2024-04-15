4142d3697744228fe48524f7fd401e1ca1466416
Kian Musser <kian@kianmusser.com>
2024-04-15 16:25:42 -0500

    messy commit

--- generator/generate.sh
+++ generator/generate.sh
@@ -8,10 +8,11 @@ JSON_FILE=$1
 TEMPLATE_DIR="template"
 OUTPUT_DIR="build/$(basename "$JSON_FILE" .json)"
 
+rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
-cp -r "$TEMPLATE_DIR/"* "$OUTPUT_DIR/"
+cp -r "$TEMPLATE_DIR/dynamic/"* "$OUTPUT_DIR/"
 
-find "$OUTPUT_DIR" \( -name "*.html" -o -name "*.css" -o -name "*.js" \) | while read -r file; do
+find "$OUTPUT_DIR" -type f | while read -r file; do
     echo "processing $file"
     mustache "$JSON_FILE" "$file" > "$file".tmp && mv "$file".tmp "$file"
 done
