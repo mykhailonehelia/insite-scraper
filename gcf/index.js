58d18b070c35e0fd894128075805337b4516e14e
Kian Musser <kian@kianmusser.com>
2024-04-16 05:50:05 -0500

    Refactored function to use consistent string quotes and updated file templating logic.

--- gcf/index.js
+++ gcf/index.js
@@ -1,15 +1,15 @@
-import functions from '@google-cloud/functions-framework';
-import { Storage } from '@google-cloud/storage';
-import mustache from 'mustache';
-import { Readable } from 'stream';
-import handlebars from 'handlebars';
-import { customAlphabet } from 'nanoid';
+import functions from "@google-cloud/functions-framework";
+import { Storage } from "@google-cloud/storage";
+import mustache from "mustache";
+import { Readable } from "stream";
+import handlebars from "handlebars";
+import { customAlphabet } from "nanoid";
 
-const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 10);
 
-functions.http('helloHttp', async (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(405).send('Method Not Allowed');
+functions.http("helloHttp", async (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).send("Method Not Allowed");
     return;
   }
 
@@ -17,12 +17,12 @@ functions.http('helloHttp', async (req, res) => {
   try {
     data = JSON.parse(req.body);
   } catch (error) {
-    res.status(400).send('Bad Request: Invalid JSON');
+    res.status(400).send("Bad Request: Invalid JSON");
     return;
   }
 
   const storage = new Storage();
-  const bucketName = 'your-bucket-name';
+  const bucketName = "your-bucket-name";
   let folderName;
   const bucket = storage.bucket(bucketName);
   let folderExists = true;
@@ -33,24 +33,27 @@ functions.http('helloHttp', async (req, res) => {
   } while (folderExists);
 
   const [files] = await bucket.getFiles({ prefix: folderName });
-  const [files] = await bucket.getFiles({ prefix: 'your-folder-name' });
+  const [files] = await bucket.getFiles({ prefix: "your-folder-name" });
 
-  await Promise.all(files.map(async (file) => {
-    const [content] = await file.download();
-    const template = handlebars.compile(content.toString());
-    const templatedContent = template(data);
-    const newFileName = `${folderName}/${file.name}`;
-    const newFileName = `templated/${file.name}`;
-    const fileStream = new Readable();
-    fileStream.push(templatedContent);
-    fileStream.push(null);
-    const newFile = bucket.file(newFileName);
-    await new Promise((resolve, reject) => {
-      fileStream.pipe(newFile.createWriteStream())
-        .on('error', reject)
-        .on('finish', resolve);
-    });
-  }));
+  await Promise.all(
+    files.map(async (file) => {
+      const [content] = await file.download();
+      const template = handlebars.compile(content.toString());
+      const templatedContent = template(data);
+      const newFileName = `${folderName}/${file.name}`;
+      const newFileName = `templated/${file.name}`;
+      const fileStream = new Readable();
+      fileStream.push(templatedContent);
+      fileStream.push(null);
+      const newFile = bucket.file(newFileName);
+      await new Promise((resolve, reject) => {
+        fileStream
+          .pipe(newFile.createWriteStream())
+          .on("error", reject)
+          .on("finish", resolve);
+      });
+    })
+  );
 
-  res.status(200).send('Files templated and uploaded successfully');
+  res.status(200).send("Files templated and uploaded successfully");
 });
