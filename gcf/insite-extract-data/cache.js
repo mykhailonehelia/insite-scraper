1a540e02b64d582277a410dbd4074a12a28ad64d
Kian Musser <kian@kianmusser.com>
2024-04-16 07:48:13 -0500

    add caching and pagespeed insights

--- gcf/insite-extract-data/cache.js
+++ gcf/insite-extract-data/cache.js
@@ -9,15 +9,45 @@ class GcsCache {
   }
 
   /**
-   * @param {string} key
+   * @param {string[]} key
+   * @returns {Promise<string|null>}
    */
   async get(key) {
-    const resp = await this.bucket.file(key).get();
+    const encodedKey = GcsCache.sanitizeKey(key);
+    console.log(`GET ${encodedKey}`);
+
+    try {
+      const [file] = await this.bucket.file(encodedKey).download();
+      return file.toString();
+    } catch (err) {
+      if (err.code === 404) {
+        return null;
+      } else {
+        throw err;
+      }
+    }
   }
 
   /**
-   * @param {string} key
+   * @param {string[]} key
    * @param {string} value
    */
-  async set(key, value) {}
+  async set(key, value) {
+    const encodedKey = GcsCache.sanitizeKey(key);
+    console.log(`SET ${encodedKey}`);
+
+    await this.bucket.file(encodedKey).save(value, {
+      contentType: "text/plain",
+    });
+  }
+
+  /**
+   * @param {string[]} key
+   * @returns {string}
+   */
+  static sanitizeKey(key) {
+    return key.map((keyPiece) => encodeURIComponent(keyPiece)).join("/");
+  }
 }
+
+export { GcsCache };
--- gcf/insite-extract-data/index.js
+++ gcf/insite-extract-data/index.js
@@ -1,2 +1,61 @@
 import functions from "@google-cloud/functions-framework";
-import { Bucket, Storage } from "@google-cloud/storage";
+import google from "googleapis";
+import { Storage } from "@google-cloud/storage";
+import { GcsCache } from "./cache.js";
+
+functions.http("entry", async (req, res) => {
+  /** @type {string} */
+  const url = req.query.url;
+  if (url === undefined) {
+    throw new Error("Please provide a URL.");
+  }
+
+  const storage = new Storage();
+  const cache = new GcsCache(storage.bucket("project-insite-cache"));
+
+  const psRes = await runPageSpeed(url, cache);
+
+  res.status(200).send(JSON.stringify(psRes, null, 2));
+});
+
+/**
+ * @param {string} url
+ * @param {GcsCache} cache
+ */
+async function runPageSpeed(url, cache) {
+  const cacheKey = ["pagespeed", url];
+  const cacheResp = await cache.get(cacheKey);
+  let rawResponse;
+  if (cacheResp !== null) {
+    rawResponse = JSON.parse(cacheResp);
+  } else {
+    const client = new google.pagespeedonline_v5.Pagespeedonline({});
+    const options = {
+      category: ["BEST_PRACTICES", "PERFORMANCE", "ACCESSIBILITY", "SEO"],
+      url,
+      strategy: "mobile",
+    };
+
+    const resp = await client.pagespeedapi.runpagespeed(options);
+    rawResponse = resp.data;
+
+    await cache.set(cacheKey, JSON.stringify(rawResponse));
+  }
+
+  const lhRes = rawResponse.lighthouseResult;
+  if (lhRes === undefined) {
+    throw new Error(
+      `Received invalid response running pagespeed for url: ${url}`
+    );
+  }
+
+  const result = {
+    url: lhRes.finalUrl,
+    performance: lhRes.categories?.performance?.score,
+    accessibility: lhRes.categories?.accessibility?.score,
+    best_practices: lhRes.categories?.["best-practices"]?.score,
+    seo: lhRes.categories?.seo?.score,
+  };
+
+  return result;
+}
