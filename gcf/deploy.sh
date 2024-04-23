8ecc26ccc2cafa6e6632c8c49d0db4d6be1ee34f
Kian Musser <kian@kianmusser.com>
2024-04-23 07:22:18 -0500

    Added environment variable setting for specific function deployment and allowed unauthenticated access in deploy script, and updated JavaScript in dashboard.

--- gcf/deploy.sh
+++ gcf/deploy.sh
@@ -6,7 +6,11 @@ for dir in "$SCRIPT_DIR"/*; do
   if [ -d "$dir" ]; then
     FOLDER_NAME=$(basename "$dir")
     echo "Starting deployment for function: $FOLDER_NAME"
-    (cd "$dir" && gcloud functions deploy "$FOLDER_NAME" --gen2 --runtime=nodejs20 --source=. --entry-point=entry --trigger-http) &
+	envVars=""
+	if [ "$dir" = "insite-extract-data" ]; then
+		envVars="--set-env-vars SCRAPINGBEE_API_KEY=$SCRAPINGBEE_API_KEY"
+	fi
+    (cd "$dir" && gcloud functions deploy "$FOLDER_NAME" --gen2 --runtime=nodejs20 --source=. --entry-point=entry --trigger-http --allow-unauthenticated --quiet $envVars) &
   fi
 done
 
